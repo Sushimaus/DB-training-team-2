597e990e00f1bb05c75442a544eb056044c752da
Sushimaus <szd260@gmail.com>
2026-07-27 14:10:59 +0200

    local changes

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-25T15:04:26+0530",
-    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.7 (Amazon.com Inc.)"
+    date = "2026-07-27T13:53:15+0200",
+    comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
 public class TradeMapperImpl implements TradeMapper {
